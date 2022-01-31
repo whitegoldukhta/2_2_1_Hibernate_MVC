924e83b5251788fb3242465da65f0d4268d19ff7
whitegoldukhta <james.99@list.ru>
2022-01-31 18:15:48 +0200

    spring

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+
 import javax.persistence.TypedQuery;
 import java.util.List;
 
